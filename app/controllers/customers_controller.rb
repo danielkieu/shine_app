391b0119b73117551ea158c294628b79526a30b2
kieu.viet.hung <kieu.viet.hung@framgia.com>
2016-12-22 06:45:40 +0700

    update

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,10 @@
 class CustomersController < ApplicationController
   def index
-    @customers = Customer.all.limit(10)
+    if params[:key_words].present?
+      customer_search_term = CustomerSearchTerm.new(params[:key_words])
+      @customers = Customer.where(customer_search_term.where_clause, customer_search_term.where_args).order(customer_search_term.order).limit(20)
+    else
+      @customers = []
+    end
   end
 end
